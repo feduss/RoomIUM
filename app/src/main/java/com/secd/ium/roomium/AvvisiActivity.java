bc5116c205d6781a39f970dfe2c8b5de813ff238
Federico <feduss@live.it>
2018-01-22 17:09:44 +0100

    Prove

--- app/src/main/java/com/secd/ium/roomium/AvvisiActivity.java
+++ app/src/main/java/com/secd/ium/roomium/AvvisiActivity.java
@@ -1,15 +1,45 @@
 package com.secd.ium.roomium;
 
 import android.support.design.widget.TabLayout;
-import android.support.v4.view.ViewPager;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.TextView;
 
 public class AvvisiActivity extends AppCompatActivity {
+
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    //private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //TAB
 
         // Set the content of the activity to use the AvvisiActvity.xml layout file
         setContentView(R.layout.activity_avvisi);
@@ -26,5 +56,8 @@ public class AvvisiActivity extends AppCompatActivity {
         // Give the TabLayout the ViewPager
         TabLayout tabLayout = (TabLayout) findViewById(R.id.category_list);
         tabLayout.setupWithViewPager(viewPager);
+
     }
+
+
 }
