3c3f47e141c75cd07767dceee2c1c99f85bbe89c
Federico <feduss@live.it>
2018-01-22 21:20:01 +0100

    LISTA FUNZIONANTE!!!!

--- app/src/main/java/com/secd/ium/roomium/AvvisiActivity.java
+++ app/src/main/java/com/secd/ium/roomium/AvvisiActivity.java
@@ -17,6 +17,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class AvvisiActivity extends AppCompatActivity {
@@ -56,6 +58,8 @@ public class AvvisiActivity extends AppCompatActivity {
         // Give the TabLayout the ViewPager
         TabLayout tabLayout = (TabLayout) findViewById(R.id.category_list);
         tabLayout.setupWithViewPager(viewPager);
+
+
 
     }
 
--- app/src/main/java/com/secd/ium/roomium/AvvisiFragment.java
+++ app/src/main/java/com/secd/ium/roomium/AvvisiFragment.java
@@ -3,15 +3,23 @@ package com.secd.ium.roomium;
 
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class AvvisiFragment extends android.support.v4.app.Fragment {
+public class AvvisiFragment extends ListFragment {
 
 
     public AvvisiFragment() {
@@ -21,8 +29,25 @@ public class AvvisiFragment extends android.support.v4.app.Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup                  container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.activity_avvisi, container, false);
-        return rootView;
-    }
+        View fragmentLayout = inflater.inflate(R.layout.fragment_avvisi, container, false);
 
+        //ListView listView1 = (ListView) fragmentLayout.findViewById(R.id.lista);
+        String[] array= {"titolo", "sottotitolo", "silvio", "puzza", "poco"};
+
+        List<String> array1= new ArrayList();
+
+        for(int i=0; i<array.length; i++){
+            array1.add(array[i]);
+        }
+
+        //quello dopo r.layout è il design generico con 2 righe
+        ArrayAdapter<String> adapter = new ArrayAdapter(getActivity(), R.layout.riga_lista, R.id.titolo_riga, array1);
+        ListView listView = (ListView)fragmentLayout.findViewById(R.id.lista);
+        listView.setAdapter(adapter);
+
+        return listView; //BAGASSSSAAAA DI UNA LIST VIEW DEMMERDA
+        
+
+    }
 }
+
