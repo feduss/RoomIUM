178397c49eeb19e4ba3f24723a7ab6c7061cd9f6
Tommaso <tommasotroja@outlook.it>
2018-02-10 14:14:59 +0100

    Impostazioni pagina di prenotazione (eccetto la selezione dell'ora) e modifiche minime.

--- app/src/main/java/com/secd/ium/roomium/PrenotaFragment.java
+++ app/src/main/java/com/secd/ium/roomium/PrenotaFragment.java
@@ -1,12 +1,19 @@
 package com.secd.ium.roomium;
 
 
+import android.app.AlertDialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Build;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -20,8 +27,52 @@ public class PrenotaFragment extends android.support.v4.app.Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup                  container, Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.activity_prenota, container, false);
+
+        Spinner aula = (Spinner) rootView.findViewById(R.id.spinneraula);
+        String[] itemsAula = new String[]{"Seleziona l'aula","A", "B", "C", "Laboratorio T", "D", "E", "F", "Laboratorio M", "Matematica", "Fisica"};
+        ArrayAdapter<String> adapterAula = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_dropdown_item, itemsAula);
+        aula.setAdapter(adapterAula);
+
+        Spinner materia = (Spinner) rootView.findViewById(R.id.spinnermateria);
+        String[] itemsMateria = new String[]{"Seleziona la materia","PR1","MD", "FDI", "CDI", "ASD", "ARE"};
+        ArrayAdapter<String> adapterMateria = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_dropdown_item, itemsMateria);
+        materia.setAdapter(adapterMateria);
+
+        Button data = (Button) rootView.findViewById(R.id.datebutton);
+
+        Button conferma = (Button) rootView.findViewById(R.id.prenotaconfirm);
+
+        data.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                DialogFragment newFragment = new DatePickerFragment();
+                newFragment.show(getActivity().getFragmentManager(), "datePicker");
+            }
+        });
+
+        conferma.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                AlertDialog.Builder builder;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    builder = new AlertDialog.Builder(getContext(), android.R.style.Theme_Material_Dialog_Alert);
+                } else {
+                    builder = new AlertDialog.Builder(getContext());
+                }
+
+                builder.setTitle("Prenotare l'aula selezionata?");
+                builder.setPositiveButton("Conferma", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getActivity(), "Aula prenotata", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                builder.setNegativeButton("Annulla", null);
+                AlertDialog alert = builder.create();
+                alert.show();
+            }
+        });
+
         return rootView;
     }
 }
